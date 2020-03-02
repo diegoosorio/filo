749cd33c88156cbfa7deac954ebafc187ad7af0c
Diego Osorio <diegoso-0989@hotmail.com>
2020-03-01 21:40:19 -0500

    Listas

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
 import React, {Component} from 'react';
 import {Text} from 'react-native';
 import Home from './src/screens/containers/Home';
-import Header from './src/sections/components/Header'
+import Header from './src/sections/components/Header';
+import SuggestionList from './src/videos/containers/SuggestionList';
 
 export default class App extends Component {
   render() {
@@ -10,7 +11,7 @@ export default class App extends Component {
         <Header />
         <Text>Buscador</Text>
         <Text>Categorias</Text>
-        <Text>Sugerencias</Text>
+        <SuggestionList />
       </Home>
     );
   }
