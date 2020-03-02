52e68d623f35b91b12b4bd99e45264fa31118796
Diego Osorio <diegoso-0989@hotmail.com>
2020-03-01 21:32:09 -0500

    Soporte a iphone X en el header

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import React, {Component} from 'react';
 import {Text} from 'react-native';
 import Home from './src/screens/containers/Home';
+import Header from './src/sections/components/Header'
 
 export default class App extends Component {
   render() {
     return (
       <Home>
-        <Text>Header</Text>
+        <Header />
         <Text>Buscador</Text>
         <Text>Categorias</Text>
         <Text>Sugerencias</Text>
