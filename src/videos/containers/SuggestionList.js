2ded8daf0adaf6c1833d577f42202847728c1ec9
Diego Osorio <diegoso-0989@hotmail.com>
2020-03-02 20:27:06 -0500

    Separadores y listas

--- src/videos/containers/SuggestionList.js
+++ src/videos/containers/SuggestionList.js
@@ -1,23 +1,33 @@
 import React, {Component} from 'react';
 import {FlatList, Text} from 'react-native';
 import Layout from '../components/SuggestionListLayout';
+import Empty from '../components/Empty';
+import Separator from '../components/VerticalSeparator';
 
 class SuggestionList extends Component {
+  renderEmtpy = () => <Empty text="No hay sugerencias :(" />;
+  itemSeparator = () => <Separator />;
   render() {
     const list = [
       {
-        title: 'titulo',
+        title: 'Avengers',
         key: '1',
       },
       {
-        title: 'otro titulo',
+        title: 'Pokemon',
         key: '2',
+      },
+      {
+        title: 'Pokemon',
+        key: '3',
       },
     ];
     return (
       <Layout title="Recomendado para ti">
         <FlatList
           data={list}
+          ListEmptyComponent={this.renderEmtpy}
+          ItemSeparatorComponent={this.itemSeparator}
           renderItem={({item}) => <Text>{item.title}</Text>}
         />
       </Layout>
