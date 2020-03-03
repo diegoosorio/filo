b5ee9fc3f7b9849e652837ee9f028d4ab014f1a0
Diego Osorio <diegoso-0989@hotmail.com>
2020-03-02 20:05:50 -0500

    Estructurando las recomendaciones

--- src/videos/containers/SuggestionList.js
+++ src/videos/containers/SuggestionList.js
@@ -1,23 +1,26 @@
 import React, {Component} from 'react';
 import {FlatList, Text} from 'react-native';
+import Layout from '../components/SuggestionListLayout';
 
 class SuggestionList extends Component {
   render() {
     const list = [
       {
-        title: 'leo',
+        title: 'titulo',
         key: '1',
       },
       {
-        title: 'nidas',
+        title: 'otro titulo',
         key: '2',
       },
     ];
     return (
-      <FlatList
-        data={list}
-        renderItem={({item}) => <Text>{item.title}</Text>}
-      />
+      <Layout title="Recomendado para ti">
+        <FlatList
+          data={list}
+          renderItem={({item}) => <Text>{item.title}</Text>}
+        />
+      </Layout>
     );
   }
 }
