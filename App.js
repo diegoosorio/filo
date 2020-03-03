cb3c72ac52469e5e4fa1c784b56c28030011aa46
Diego Osorio <diegoso-0989@hotmail.com>
2020-03-02 21:06:25 -0500

    Peticiones fetch a servidor

--- App.js
+++ App.js
@@ -3,8 +3,13 @@ import {Text} from 'react-native';
 import Home from './src/screens/containers/Home';
 import Header from './src/sections/components/Header';
 import SuggestionList from './src/videos/containers/SuggestionList';
+import API from './utils/api';
 
 export default class App extends Component {
+  async componentDidMount(){
+    const movies = await API.getSuggestion(10);
+    console.log(movies);
+  }
   render() {
     return (
       <Home>
