41f77ceb4c9677416a7a3d6f1cbf0ae6104396ef
Diego Osorio <diegoso-0989@hotmail.com>
2020-03-04 22:35:06 -0500

    instalando y probando react-native-video

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
+/* eslint-disable react-native/no-inline-styles */
 import React, {Component} from 'react';
-import {Text} from 'react-native';
+import {Text, View} from 'react-native';
 import Home from './src/screens/containers/Home';
 import Header from './src/sections/components/Header';
 import SuggestionList from './src/videos/containers/SuggestionList';
 import CategoryList from './src/videos/containers/CategoryList';
 import API from './utils/api';
+import Video from 'react-native-video';
 
 export default class App extends Component {
   state = {
@@ -25,7 +27,30 @@ export default class App extends Component {
     return (
       <Home>
         <Header />
-        <Text>Recopilación de señas académicas en el área de filosofia</Text>
+        {/* <Text>Recopilación de señas académicas en el área de filosofia</Text> */}
+        {/* <Video source={{uri: 'https://www.youtube.com/watch?v=r-2NQzZWsaY'}} /> */}
+
+        <View
+          style={{
+            flex: 1,
+            height: 100,
+          }}>
+          <Video
+            source={{
+              uri:
+                'https://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4',
+            }}
+            style={{
+              position: 'absolute',
+              left: 0,
+              right: 0,
+              bottom: 0,
+              top: 0,
+            }}
+            resizeMode="contain"
+          />
+        </View>
+
         <Text>buscador</Text>
         <Text>categorías</Text>
         <CategoryList list={this.state.categoryList} />
